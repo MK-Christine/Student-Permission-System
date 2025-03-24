42be7075f2f3e3b217a9d285da7648a11ab9abfe
MK-Christine <mukanezachristine2001@gmail.com>
2025-03-24 15:31:57 +0200

    first commit

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost","root","","permission");
+
+
+if ($con) {
+    echo "connected sucessful";
+}
+?>
---
+++ login.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title> Login foam </title>
+        <link rel="stylesheet" href="bootstrap\css\bootstrap.min.css">
+    </head>
+    <body> 
+    <body>
+
+        <div class="container">
+
+        <div class="row mt-5">
+            <div class="col-3"></div>
+                <div class="col-6">
+                    <div class="card shadow mt-5">
+                        <div class="card-body">
+                                <h3 class="mb-4">login </h3>
+                            <form>
+                                <input type="text" placeholder="Username" class="form-control">
+                                <input type="password" placeholder="Password" class="form-control mt-2">
+                                <button type="submit" class="btn btn-primary mt-2">Login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+               
+            <div class="col-3"></div>
+            </div>
+        </div>
+
+    </body>
+        
+</html>
